7b9ab4fe640c4d1179ed0be94b3e95172d4fb12f
Abdul Rehman Memon <amemon.bscs22seecs@seecs.edu.pk>
2026-03-29 23:35:03 +0500

    complete cosway ermianl printer

--- main.go
+++ main.go
@@ -1,5 +1,98 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"unicode/utf8"
+)
+
+func tabsToSpaces(lines []string) []string {
+	var ret []string
+	for _, line := range lines {
+		l := strings.Replace(line, "\t", "    ", -1)
+		ret = append(ret, l)
+	}
+	return ret
+}
+
+func buildBalloon(lines []string, maxwidth int) string {
+	var borders []string
+	count := len(lines)
+	var ret []string
+	borders = []string{"/", "\\", "\\", "/", "|", "<", ">"}
+	top := " " + strings.Repeat("_", maxwidth+2)
+	bottom := " " + strings.Repeat("-", maxwidth+2)
+	ret = append(ret, top)
+	if count == 1 {
+		s := fmt.Sprintf("%s %s %s", borders[5], lines[0], borders[6])
+		ret = append(ret, s)
+	} else {
+		s := fmt.Sprintf(`%s %s %s`, borders[0], lines[0], borders[1])
+		ret = append(ret, s)
+		i := 1
+		for ; i < count-1; i++ {
+			s = fmt.Sprintf(`%s %s %s`, borders[4], lines[i], borders[4])
+			ret = append(ret, s)
+		}
+		s = fmt.Sprintf(`%s %s %s`, borders[2], lines[i], borders[3])
+		ret = append(ret, s)
+	}
+	ret = append(ret, bottom)
+
+	return strings.Join(ret, "\n")
+}
+
+func calculateMaxWidth(lines []string) int {
+	w := 0
+	for _, l := range lines {
+		len := utf8.RuneCountInString(l)
+		if len > w {
+			w = len
+		}
+	}
+	return w
+}
+
+func normalizeStringsLength(lines []string, maxwidth int) []string {
+	var ret []string
+	for _, l := range lines {
+		s := l + strings.Repeat(" ", maxwidth-utf8.RuneCountInString(l))
+		ret = append(ret, s)
+	}
+	return ret
+}
+
 func main() {
+	info, _ := os.Stdin.Stat()
+	if info.Mode()&os.ModeCharDevice != 0 {
+		fmt.Println("The command is intended to work with pipes. .. install fortune to generate text for you")
+		fmt.Println("Usage: fortune | gocowsay")
+		return
+	}
+	var lines []string
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil && err == io.EOF {
+			break
+		}
+		lines = append(lines, string(line))
+	}
+	var cow = `         \  ^__^
+          \ (oo)\_______
+	    (__)\       )\/\
+	        ||----w |
+	        ||     ||
+		`
+	lines = tabsToSpaces(lines)
+	maxwidth := calculateMaxWidth(lines)
+	messages := normalizeStringsLength(lines, maxwidth)
+	balloon := buildBalloon(messages, maxwidth)
+	fmt.Println(balloon)
+	fmt.Println(cow)
+	fmt.Println()
 
 }
